d2c70bff4b692cbab0ee8ddd7116af2013f8d320
Kelbeans <kaimalabanan@gmail.com>
2026-02-23 10:31:42 +0800

    fix: Configure Terraform for ap-southeast-2 region with terraform_sl10 profile
    - Use terraform_sl10 AWS CLI profile
    - Update region to ap-southeast-2 (Sydney, Australia)
    - Update AMI ID for ap-southeast-2 region (Ubuntu 22.04 LTS)
    - Add aws_profile variable for flexibility

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,13 @@
 variable "aws_region" {
   description = "AWS region to deploy to"
   type        = string
-  default     = "ap-southeast-1" # Singapore (closest to Philippines)
+  default     = "ap-southeast-2" # Sydney, Australia
+}
+
+variable "aws_profile" {
+  description = "AWS CLI profile to use"
+  type        = string
+  default     = "terraform_sl10"
 }
 
 variable "instance_type" {
@@ -13,9 +19,11 @@ variable "instance_type" {
 variable "ami_id" {
   description = "Ubuntu 22.04 LTS AMI ID for the region"
   type        = string
-  # Default: Ubuntu 22.04 LTS in ap-southeast-1 (Singapore)
-  default = "ami-0497a974f8d5dcef8"
-  # Note: AMI IDs vary by region. Update if using different region:
+  # Default: Ubuntu 22.04 LTS in ap-southeast-2 (Sydney)
+  default = "ami-0310483fb2b488153"
+  # Note: AMI IDs vary by region. Common regions:
+  # ap-southeast-1 (Singapore): ami-0497a974f8d5dcef8
+  # ap-southeast-2 (Sydney): ami-0310483fb2b488153
   # us-east-1: ami-0c7217cdde317cfec
   # us-west-2: ami-0efcece6bed30fd98
   # eu-west-1: ami-0905a3c97561e0b69
